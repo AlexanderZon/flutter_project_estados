287ca8d86c2ee860fcb380be191559a70ceae596
AlexanderZon <amontenegro.sistemas@gmail.com>
2023-12-17 14:34:16 -0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:project_estados/pages/pagina1_page.dart';
+import 'package:project_estados/pages/pagina2_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'pagina2',
+      routes: {
+        'pagina1': (_) => Pagina1Page(),
+        'pagina2': (_) => Pagina2Page(),
+      },
+    );
+  }
+}
